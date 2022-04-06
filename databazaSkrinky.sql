e31800dcbe31ae1c839e878408441ad747a1a494
DanielSimek548 <73018736+DanielSimek548@users.noreply.github.com>
2022-04-06 21:17:27 +0200

    Add files via upload

---
+++ databazaSkrinky.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE skrinky;
+
+CREATE TABLE `sekciaa` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`surname` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`class` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`id`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=0
+;
