213de4230936c42923b7e6a05089954bc26cbad3
DanielSimek548 <73018736+DanielSimek548@users.noreply.github.com>
2022-04-07 08:13:55 +0200

    Add files via upload

--- databazaSkrinky.sql
+++ databazaSkrinky.sql
@@ -5,6 +5,7 @@ CREATE TABLE `sekciaa` (
 	`name` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
 	`surname` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
 	`class` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`number` INT(11) NOT NULL,
 	PRIMARY KEY (`id`) USING BTREE
 )
 COLLATE='utf8mb4_general_ci'
