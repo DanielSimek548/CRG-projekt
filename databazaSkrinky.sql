720c8b1f2c22fb3864d5c850331123a1582bb405
Denisek1989 <77125880+Denisek1989@users.noreply.github.com>
2022-05-03 20:49:12 +0200

    Update databazaSkrinky.sql

--- databazaSkrinky.sql
+++ databazaSkrinky.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE skrinky;
-
-CREATE TABLE `sekciaa` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	`surname` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	`class` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	`number` INT(11) NOT NULL,
-	PRIMARY KEY (`id`) USING BTREE
+CREATE TABLE sekciaa (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+    surname VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+    class VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+    number INT(11) NOT NULL,
+    date DATE NULL DEFAULT curdate(),
+    PRIMARY KEY (id) USING BTREE
 )
 COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
