d36713d13b05fefef7e59c206a4d887b78d3b051
SamuelVnuk <73060091+SamuelVnuk@users.noreply.github.com>
2022-06-01 14:46:01 +0200

    Delete Skrinky_2.0/Skrinky 2.0 directory

--- Skrinky_2.0/Skrinky 2.0/dist/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"NewJFrame"},{"p":"<Unnamed>","l":"TabulkaFrame"},{"p":"<Unnamed>","l":"User"}];updateSearchResults();
